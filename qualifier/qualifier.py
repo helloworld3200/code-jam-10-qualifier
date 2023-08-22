d9464116207ac11818457acc92950c277bd67fc5
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2023-08-22 11:32:51 +0100

    update qualifier.py

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -2,13 +2,23 @@ import PIL as pil
 
 def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
     """
-    Return True if the given input allows the rearrangement of the image, False otherwise.
+    Return True if the given input allows the rearrangement of the image, False otherwise. done
 
     The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
     once.
     """
     if image_size[0]%tile_size[0] or image_size[1]%tile_size[1]:
         return False
+    
+    tile_count = image_size[0]/tile_size[0]*image_size[1]/tile_size[1]
+    sorted_order = sorted(ordering)
+
+    if sorted_order[-1] != tile_count:
+        return False
+    elif len(sorted_order) > len(set(sorted_order)):
+        return False
+    
+    return True
 
 
 def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
